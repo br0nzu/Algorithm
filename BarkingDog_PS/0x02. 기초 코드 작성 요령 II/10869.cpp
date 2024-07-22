b42ce1e703936babacf99bdae255d0a300162dd9
br0nzu <dongpago2@gmail.com>
2024-07-23 00:40:30 +0900

    사칙연산

---
+++ BarkingDog_PS/0x02. 기초 코드 작성 요령 II/10869.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(0);
+
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << "\n";
+	cout << A - B << "\n";
+	cout << A * B << "\n";
+	cout << A / B << "\n";
+	cout << A % B;
+}
