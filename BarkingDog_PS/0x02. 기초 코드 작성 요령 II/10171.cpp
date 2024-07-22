63bce33adf5df1e424bfeb0c9334e359a3f8d276
br0nzu <dongpago2@gmail.com>
2024-07-23 00:39:52 +0900

    Cat

---
+++ BarkingDog_PS/0x02. 기초 코드 작성 요령 II/10171.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(0);
+
+	cout << "\\    /\\" << "\n";
+	cout << " )  ( \')" << "\n";
+	cout << "(  /  )" << "\n";
+	cout << " \\(__)|";
+}
