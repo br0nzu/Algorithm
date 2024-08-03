fcc89dda4b751a1d1d16d4aba0277698d7073279
br0nzu <dongpago2@gmail.com>
2024-08-03 20:28:00 +0900

    숫자의 개수

---
+++ BarkingDog_PS/0x03. 배열/2577.cpp
@@ -0,0 +1,45 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10], x;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int A, B, C;
+	cin >> A >> B >> C;
+
+	int result = A * B * C;
+
+	while (result > 0) {
+		x = result % 10;
+		arr[x]++;
+		result /= 10;
+	}
+
+	for (int i = 0; i < 10; i++) cout << arr[i] << "\n";
+}
+
+// sol
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[10] = {};
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int A, B, C;
+  cin >> A >> B >> C;
+  int t=A*B*C;
+
+  // 계산 결과를 자릿수별로 확인하여 저장
+  while (t>0) {
+	d[t%10]++;
+	t/=10;
+  }
+  for (int i=0; i<10; ++i) cout << d[i] << '\n';
+}
+*/
